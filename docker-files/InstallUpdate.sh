ba07ec6a330a24f63ccbecf7341f3bf7778dad3e
Vox Draco <vox@voxdraconis.co.uk>
2021-07-29 18:15:29 +0100

    Added initial working files

---
+++ docker-files/InstallUpdate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/steam/steamcmd +@sSteamCmdForcePlatformType linux +login anonymous +force_install_dir /home/steam +app_update 896660 validate +quit
---
+++ docker-files/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export templdpath=$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
+export SteamAppID=892970
+
+/home/steam/steamcmd +@sSteamCmdForcePlatformType linux +login anonymous +force_install_dir /home/steam +app_update 896660 validate +quit
+
+echo "Starting server PRESS CTRL-C to exit"
+/home/steam/valheim_server.x86_64 -name ${SERVERNAME} -port ${PORT} -world "Dedicated" -password ${PASSWORD} -public 1
+export LD_LIBRARY_PATH=$templdpath
