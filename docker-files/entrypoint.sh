cdf7dc75364b058e38d9bf49fd95a8d296d708e6
voxdraco <88157547+voxdraco@users.noreply.github.com>
2021-07-31 18:35:04 +0100

    Update entrypoint.sh

--- docker-files/entrypoint.sh
+++ docker-files/entrypoint.sh
@@ -5,6 +5,6 @@ export SteamAppID=892970
 
 /home/steam/steamcmd +@sSteamCmdForcePlatformType linux +login anonymous +force_install_dir /home/steam +app_update 896660 validate +quit
 
-echo "Starting server PRESS CTRL-C to exit"
+
 /home/steam/valheim_server.x86_64 -name ${SERVERNAME} -port ${PORT} -world "Dedicated" -password ${PASSWORD} -public 1
 export LD_LIBRARY_PATH=$templdpath
